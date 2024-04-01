456380fc6b32763f5d79d34c8936a4a97ae2144e
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-04-01 08:50:35 +0300

    Add files via upload

---
+++ spamxs.sh
@@ -0,0 +1,47 @@
+echo "Welcome the tool spamxs"
+##############
+echo "Follow me on YouTube on my channel orphan hacker"
+##############
+echo "Follow me on my Instagram account that is ahu_orphan"
+#############
+echo "Enter the password:"
+############
+read user
+########### 
+if [ $user == orphan ]
+#############
+then
+#############
+echo "The password is correct"
+#############
+echo "Enter the country code:"
+###############
+read x
+##############
+echo "Enter the victim's number:"
+###########
+read y
+###########
+echo "Press Enter to complete the process:"
+##############
+read v
+###########
+echo "Enter the number of times you want to call:"
+###########
+read g
+############
+echo "Enter the length of time you want between each connection:"
+############
+read f
+##########
+echo "Press Enter to complete the process:"
+########
+read h
+#########
+echo "The victim is being contacted now. Please wait..."
+#################
+read l
+############
+echo "The connections were made successfully"
+fi
+	exit
